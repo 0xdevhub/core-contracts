d065afc709b6cd0b418e9fc94b2e26c8e70ae16b
Welliton Gervickas <wellitogervickas@gmail.com>
2023-11-13 15:38:30 -0300

    chore: remove migrated code

--- contracts/apps/crosschain-nft/NFT.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract MyToken is ERC721 {
-    constructor() ERC721("MyToken", "MTK") {}
-}
